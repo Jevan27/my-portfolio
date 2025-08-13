7798a5d385e8f713de5b330040d87eace03d96df
Jevan27 <campillosjevanlloyd27@gmail.com>
2025-08-13 14:11:39 +0800

    Update

--- src/data/Expertise.js
+++ src/data/Expertise.js
@@ -12,42 +12,42 @@ export const expertiseData = [
     id: 0,
     expertise: "React",
     image: image1, // Place inside public/images/
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I’m a web developer who loves creating cool and interactive sites with React.js. I enjoy experimenting with new ideas and making projects that are fun, smooth, and easy to use."
   },
   {
     id: 1,
     expertise: "Node.js",
     image: image2,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I’m a backend tinkerer who enjoys building fast and reliable apps with Node.js. I like turning ideas into smooth-running servers that just work."
   },
   {
     id: 2,
     expertise: "Tailwind CSS",
     image: image3,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I love using Tailwind CSS to bring designs to life quickly and beautifully. It’s my go-to for making clean, responsive layouts without all the extra hassle."
   },
    {
     id: 3,
     expertise: "Java",
     image: image4, // Place inside public/images/
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I enjoy coding in Java for its reliability and versatility. Whether it’s building apps, solving problems, or just experimenting, I like how Java lets me create solid and scalable solutions."
   },
    {
     id: 4,
     expertise: "Vite",
     image: image5, // Place inside public/images/
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I like using Vite because it makes building projects super fast and smooth. It’s my favorite tool for quickly spinning up apps and seeing changes instantly."
   },
    {
     id: 5,
     expertise: "PHP",
     image: image6, // Place inside public/images/
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    description: "I enjoy working with PHP to build dynamic and functional websites. It’s a straightforward tool that lets me bring ideas to life on the web."
   },
    {
     id: 6,
     expertise: "Git",
     image: image7, // Place inside public/images/
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing eslit."
+    description: "I use Git to keep my projects organized and my code safe. It’s like my time machine for coding helping me track changes and work smoothly with others."
   },
 ];
